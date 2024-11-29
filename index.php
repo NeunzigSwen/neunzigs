6fd41a0f87b840a88b2207a52495c81fd3fb8df7
Your Name <you@example.com>
2024-11-29 12:12:02 +0100

    Start Point

---
+++ index.php
@@ -0,0 +1,344 @@
+<?php require_once('./assets/layout/header.php'); ?> 
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section dark-background">
+
+      <img src="assets/img/planet.jpg" alt="" data-aos="fade-in" class="">
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+        <h2>Swen Neunzig</h2>
+        <p>Ich bin <span class="typed" data-typed-items="begeisterungsfähig, neugierig, motiviert, teamfähig, zuverlässig">begeisterungsfähig</span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span><span class="typed-cursor typed-cursor--blink" aria-hidden="true"></span></p>
+      </div>
+
+    </section><!-- /Hero Section -->
+
+    <!-- About Section -->
+    <section id="about" class="about section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Über mich</h2>
+        <p>"Seit meiner jüngsten Jugend bin ich an Technik interessiert. Es fing mit einem Strombaukasten an, ging über die Anfänge mit dem C64 und Amiga bis hin zur heutigen Computertechnik."</p>
+      </div><!-- End Section Title -->
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4 justify-content-center">
+          <div class="col-lg-4">
+            <img src="assets/img/my-profile-img.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 content">
+            <h2>Fachinformatiker für Systemintegration in der Ausbildung</h2>
+            <p class="fst-italic py-3">
+              Meine Persönlichen Daten:
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Geboren am:</strong> <span>9 August 1978</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Telefon:</strong> <span>+49 176-21821902</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Stadt:</strong> <span>Krefeld,NRW</span></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Alter:</strong> <span>46</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>neunzigswen@hotmail.de</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Verfügbar für:</strong> <span>Praktikum</span></li>
+                </ul>
+              </div>
+            </div>
+            <p class="py-3">
+              <p>Im Folgenden möchte ich Ihnen meine Stärken vorstellen, die mich zu einem idealen Kandidaten für die Stelle als Praktikant machen :</p>
+
+              <p>Begeisterungsfähig: Ich habe die Fähigkeit, mich schnell für neue Projekte und Aufgaben zu begeistern und meine Motivation auf mein Umfeld zu übertragen.</p>
+              
+              <P>Neugierig: Meine unersättliche Neugier treibt mich an, ständig Neues zu lernen und mich weiterzuentwickeln. Diese Eigenschaft hilft mir, innovative Lösungen zu finden und mich rasch in neue Themengebiete einzuarbeiten.</P>
+              
+              <p>Motiviert: Ich bin hochmotiviert, meine Ziele zu erreichen und Projekte erfolgreich abzuschließen. Diese Motivation spiegelt sich in meiner Arbeitseinstellung und meinem Engagement wider.</p>
+              
+              <p>Teamfähig: Ich arbeite gerne im Team und trage aktiv zu einer positiven und produktiven Arbeitsatmosphäre bei. Meine Fähigkeit zur klaren und effektiven Kommunikation unterstützt die Zusammenarbeit im Team.</p>
+              
+              <p>Zuverlässig: Auf meine Zuverlässigkeit können sich Kollegen und Vorgesetzte stets verlassen. Ich erfülle meine Aufgaben gewissenhaft und termingerecht, was mich zu einem verlässlichen Teammitglied macht.</p>
+              
+              <p>Ich freue mich darauf, diese Stärken in Ihrem Unternehmen einzubringen und gemeinsam erfolgreich zu sein.</p>
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+    </section><!-- /About Section -->
+
+  
+
+    <!-- Skills Section -->
+    <section id="skills" class="skills section light-background">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Skills</h2>
+        <p>Meine Computer Skills sind</p>
+      </div><!-- End Section Title -->
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row skills-content skills-animation">
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill"><span>Netzwerk </span> <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div><!-- End Skills Item -->
+
+            <div class="progress">
+              <span class="skill"><span>c++</span> <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div><!-- End Skills Item -->
+
+            <div class="progress">
+              <span class="skill"><span>Linux</span> <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div><!-- End Skills Item -->
+
+          </div>
+
+          <div class="col-lg-6">
+
+            <div class="progress">
+              <span class="skill"><span>Word/Excel/Powerpoint</span> <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div><!-- End Skills Item -->
+
+            <div class="progress">
+              <span class="skill"><span>Hardware</span> <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div><!-- End Skills Item -->
+
+            <div class="progress">
+              <span class="skill"><span>Html/Css</span> <i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div><!-- End Skills Item -->
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section><!-- /Skills Section -->
+
+    <!-- Resume Section -->
+    <section id="resume" class="resume section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Resume</h2>
+        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+      </div><!-- End Section Title -->
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">Sumary</h3>
+
+            <div class="resume-item pb-0">
+              <h4>Brandon Johnson</h4>
+              <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
+              <ul>
+                <li>Portland par 127,Orlando, FL</li>
+                <li>(123) 456-7891</li>
+                <li>alice.barkley@example.com</li>
+              </ul>
+            </div><!-- Edn Resume Item -->
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>Master of Fine Arts &amp; Graphic Design</h4>
+              <h5>2015 - 2016</h5>
+              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+              <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
+              <h5>2010 - 2014</h5>
+              <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+              <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+            </div><!-- Edn Resume Item -->
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>Senior graphic design specialist</h4>
+              <h5>2019 - Present</h5>
+              <p><em>Experion, New York, NY </em></p>
+              <ul>
+                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+              </ul>
+            </div><!-- Edn Resume Item -->
+
+            <div class="resume-item">
+              <h4>Graphic design specialist</h4>
+              <h5>2017 - 2018</h5>
+              <p><em>Stepping Stone Advertising, New York, NY</em></p>
+              <ul>
+                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+              </ul>
+            </div><!-- Edn Resume Item -->
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section><!-- /Resume Section -->
+
+    <!-- Portfolio Section -->
+    <section id="portfolio" class="portfolio section light-background">
+   
+          <!-- Testimonials Section -->
+        <section id="testimonials" class="testimonials section light-background">
+
+          <!-- Section Testimonials -->
+            <div class="container section-title" data-aos="fade-up">
+                <h2>Registrierung</h2>
+              <form action="assets/php/registerToSql.php" method="post">
+                <label>Männlich</label>
+                <input type="radio" value="Männlich" name="gender" />
+                <label>Weiblich</label>
+                <input type="radio" value="Weiblich" name="gender" />
+                <label>Unheimlich</label>
+                <input type="radio" value="Unheimlich" name="gender" />
+                <br><br>
+                <input type="text" name="vorname" placeholder="Vorname" required />
+                <br /><br />
+                <input type="text" name="nachname" placeholder="Nachname" required /><br /><br />
+                <input type="email" name="email" placeholder="E-Mail" required /><br /><br />
+                <input type="password" name="passwort" placeholder="Passwort" required /><br /><br />
+                <input type="password" name="passwort_bestaetigen" placeholder="Passwort bestätigen" required /><br /><br />
+                
+                <button type="submit" class="btn btn-primary">Registrieren</button>
+              </form>
+            </div>     
+         </section><!-- /Testimonials Section -->
+
+     
+
+   
+
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact section">
+
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Kontakt</h2>
+        <p>Bei Interesse melden Sie sich gern </p>
+      </div><!-- End Section Title -->
+
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-5">
+
+            <div class="info-wrap">
+              <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
+                <i class="bi bi-geo-alt flex-shrink-0"></i>
+                <div>
+                  <h3>Addresse</h3>
+                  <p>Vulkanstraße 174 , 47807 Krefeld / NRW</p>
+                </div>
+              </div><!-- End Info Item -->
+
+              <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
+                <i class="bi bi-telephone flex-shrink-0"></i>
+                <div>
+                  <h3>Telefon</h3>
+                  <p>0176-21821902</p>
+                </div>
+              </div><!-- End Info Item -->
+
+              <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+                <i class="bi bi-envelope flex-shrink-0"></i>
+                <div>
+                  <h3>Email</h3>
+                  <p>Neunzigswen@hotmail.de</p>
+                </div>
+              </div><!-- End Info Item -->
+
+               </div>
+          </div>
+
+          <div class="col-lg-7">
+            <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
+              <div class="row gy-4">
+
+                <div class="col-md-6">
+                  <label for="name-field" class="pb-2">Ihr Name</label>
+                  <input type="text" name="name" id="name-field" class="form-control" required="">
+                </div>
+
+                <div class="col-md-6">
+                  <label for="email-field" class="pb-2">Ihre Email Adresse</label>
+                  <input type="email" class="form-control" name="email" id="email-field" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <label for="subject-field" class="pb-2">Ihr Anliegen</label>
+                  <input type="text" class="form-control" name="subject" id="subject-field" required="">
+                </div>
+
+                <div class="col-md-12">
+                  <label for="message-field" class="pb-2">Ihre Nachricht</label>
+                  <textarea class="form-control" name="message" rows="10" id="message-field" required=""></textarea>
+                </div>
+
+                <div class="col-md-12 text-center">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Deine Nachricht wurde versendet. Vielen Dank!</div>
+
+                  <button type="submit">Nachricht Gesendet</button>
+                </div>
+
+              </div>
+            </form>
+          </div><!-- End Contact Form -->
+
+        </div>
+
+      </div>
+
+    </section><!-- /Contact Section -->
+
+  </main>
+
+<?php require_once('./assets/layout/footer.php'); ?>
